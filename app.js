cba2aaa753452abf135a96dcaacb25d3f7f8e00a
Yofandra <drayofan@gmail.com>
2024-05-26 18:03:32 +0700

    add sequelize

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 import dotenv from "dotenv";
 import connection from "./models/connection.js";
 import loggingMiddleware from "./middlewares/loggingMiddleware.js";
+import sequelize from "./models/connection.js";
 
 dotenv.config();
 
@@ -18,11 +19,18 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-connection.getConnection((err) => {
-  if (err) {
-    console.log("Error connecting to mysql :", err);
-    server.close();
-  } else {
-    console.log("Connect to mysql successfully");
-  }
-});
+// connection.getConnection((err) => {
+//   if (err) {
+//     console.log("Error connecting to mysql :", err);
+//     server.close();
+//   } else {
+//     console.log("Connect to mysql successfully");
+//   }
+// });
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -5,5 +5,6 @@ export default{
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USERNAME,
     PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME
+    DB: process.env.DB_NAME,
+    DIALECT: process.env.DB_DIALECT
 }
