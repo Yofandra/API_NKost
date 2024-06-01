216296da606e2206784cd4335b77b67396fc9d0a
Fahri Dwi Hariyadi <11211037@student.itk.ac.id>
2024-06-02 03:47:19 +0800

    Mengubah roleAccess menjadi StatusAccess dan menambahkan fitur apakah akun sudah diverifikasi atau belum

--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { findAll, getUserById, deleteUser, updateUser } from "../controllers/profile-controller.js";
 import verifyToken from "../middlewares/authJwt.js";
+import statusAccess from "../middlewares/statusAccess.js";
 const profileRoute = Router();
 
-profileRoute.get("/", verifyToken, findAll);
-profileRoute.get("/:id", verifyToken, getUserById);
-profileRoute.put('/:id', verifyToken, updateUser);
-profileRoute.delete("/:id", verifyToken, deleteUser);
+profileRoute.get("/", verifyToken, statusAccess, findAll);
+profileRoute.get("/:id", verifyToken, statusAccess, getUserById);
+profileRoute.put('/:id', verifyToken, statusAccess, updateUser);
+profileRoute.delete("/:id", verifyToken, statusAccess, deleteUser);
 
 export default profileRoute;
