0295bc314fabac24f3e761cb756625b21dd3345d
Yofandra <drayofan@gmail.com>
2024-06-02 23:24:40 +0700

    add authjwt

--- routes/kost-route.js
+++ routes/kost-route.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { findAll, findOne, create, update, remove } from "../controllers/kost-controller.js";
+import authJwt from "../middlewares/authJwt.js";
 const kostRoute = Router();
 
+kostRoute.use(authJwt);
 kostRoute.get("/", findAll);
 kostRoute.get("/:id", findOne);
 kostRoute.post("/", create);
