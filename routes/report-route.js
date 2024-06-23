721481fdbddacd063a935315c9171cf8d1f9bce4
Fahri Dwi Hariyadi <11211037@student.itk.ac.id>
2024-06-23 23:19:10 +0800

    menambahkan route mengambil reportbyroomid

--- routes/report-route.js
+++ routes/report-route.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createReport, updateReport, deleteReport, getReportByIdUser} from "../controllers/report-controller.js";
+import {createReport, updateReport, deleteReport, getReportByIdUser, getReportByIdRoom} from "../controllers/report-controller.js";
 import verifyToken from "../middlewares/authJwt.js";
 import { Penyewa, statusAccess } from "../middlewares/accountChecker.js"; 
 
@@ -7,6 +7,7 @@ const reportRoute = Router();
 
 reportRoute.use(verifyToken, statusAccess);
 
+reportRoute.get("/allReport", getReportByIdRoom);
 reportRoute.get("/user", getReportByIdUser);
 reportRoute.post("/", Penyewa, createReport);
 reportRoute.put("/:id", Penyewa, updateReport);
