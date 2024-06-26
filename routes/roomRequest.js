6609128dbe1a85d6b8667946fb55a95109426e18
Fahri Dwi Hariyadi <11211037@student.itk.ac.id>
2024-06-26 12:24:52 +0800

    mengubah route  api accept dan reject pada room request

--- routes/roomRequest.js
+++ routes/roomRequest.js
@@ -1,13 +1,13 @@
 import {Router} from 'express'
-import {createRoomRequest, RoomRequestAccept, RoomRequestRejected, getRoomRequest, getRoomRequestByKostId} from '../controllers/roomRequest-controller.js'
+import {createRoomRequest, RoomRequestAccept, RoomRequestReject, getRoomRequest, getRoomRequestByKostId} from '../controllers/roomRequest-controller.js'
 import {statusAccess, checkPermission, adminPermission} from "../middlewares/accountChecker.js";
 import verifyToken from '../middlewares/authJwt.js';
 const roomRequestRoute = Router()
 
 roomRequestRoute.use(verifyToken, statusAccess)
 roomRequestRoute.post('/create', createRoomRequest)
-roomRequestRoute.put('/accept', RoomRequestAccept)
-roomRequestRoute.put('/reject', RoomRequestRejected)
+roomRequestRoute.put('/:id/accept', RoomRequestAccept)
+roomRequestRoute.put('/:id/reject', RoomRequestReject)
 roomRequestRoute.get('/', getRoomRequest)
 roomRequestRoute.get('/request', getRoomRequestByKostId)
 
