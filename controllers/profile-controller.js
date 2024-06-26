97467a4d1d232a309e224f5c65b1ab07c09a0e68
Yofandra <drayofan@gmail.com>
2024-06-26 09:59:16 +0700

    add findOne

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -20,6 +20,30 @@ export const findAll = async (req, res) => {
     }
 }
 
+export const findOne = async (req, res) => {
+  const id = req.params.id;
+    console.log(id);
+    try {  
+      const user = await User.findOne({ where: { id: id } })  
+      if (!user) {
+        return res.status(404).json({
+          message: "User tidak ditemukan",
+        });
+      }
+  
+      res.status(200).json({
+        status: "Success",
+        data: user,
+      });
+  
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+  }
+
 export const getUserById = async (req, res) => {
     const id = res.locals.userId;
     console.log(id);
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { findAll, getUserById, deleteUser, updateUser, updateUserById } from "../controllers/profile-controller.js";
+import { findAll, getUserById, deleteUser, updateUser, updateUserById, findOne } from "../controllers/profile-controller.js";
 import verifyToken from "../middlewares/authJwt.js";
 import {statusAccess, checkPermission, adminPermission} from "../middlewares/accountChecker.js";
 const profileRoute = Router();
 
 profileRoute.get("/", findAll);
+profileRoute.get("/id/:id", verifyToken, statusAccess, findOne);
 profileRoute.get("/user", verifyToken, getUserById);
 profileRoute.put('/update', verifyToken, statusAccess, updateUser);
 profileRoute.put('/update/:id', verifyToken, statusAccess, adminPermission, updateUserById);
