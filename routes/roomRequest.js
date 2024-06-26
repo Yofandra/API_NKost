55609ddf02e28c94b2ce15f52b817b9ad57aacf1
Yofandra <drayofan@gmail.com>
2024-06-26 13:36:43 +0700

    fix route

--- routes/roomRequest.js
+++ routes/roomRequest.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {createRoomRequest, RoomRequestAccept, RoomRequestReject, getRoomRequest, getRoomRequestByKostId} from '../controllers/roomRequest-controller.js'
+import {createRoomRequest, RoomRequestAccept, RoomRequestRejected, getRoomRequest, getRoomRequestByKostId} from '../controllers/roomRequest-controller.js'
 import {statusAccess, checkPermission, adminPermission} from "../middlewares/accountChecker.js";
 import verifyToken from '../middlewares/authJwt.js';
 const roomRequestRoute = Router()
@@ -7,7 +7,7 @@ const roomRequestRoute = Router()
 roomRequestRoute.use(verifyToken, statusAccess)
 roomRequestRoute.post('/create', createRoomRequest)
 roomRequestRoute.put('/:id/accept', RoomRequestAccept)
-roomRequestRoute.put('/:id/reject', RoomRequestReject)
+roomRequestRoute.put('/:id/reject', RoomRequestRejected)
 roomRequestRoute.get('/', getRoomRequest)
 roomRequestRoute.get('/request', getRoomRequestByKostId)
 
